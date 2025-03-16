0eb86fcc65bc89a9031313042974d8a60e7ec152
HotShot0901 <contact.hot.shor.1234@gmail.com>
2025-03-16 11:01:00 +0530

    Basic Server

---
+++ cpp-server/server.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "httplib.h"
+
+int main() {
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request& req, httplib::Response& res) {
+        res.set_content("Hello World!", "text/plain");
+        });
+
+    svr.listen("localhost", 8080);
+
+    return 0;
+}
